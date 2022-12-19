9d9bc3f285c40c9b53b8cb1bf5a0b3f0ef395376
suujay <121003918+suujay@users.noreply.github.com>
2022-12-19 13:19:01 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showString("happy chinese new year")
+    music.playMelody("C5 B A G F E D C ", 1000)
+    music.playMelody("C D E F G A B C5 ", 1000)
+})
+basic.forever(function () {
+	
+})
